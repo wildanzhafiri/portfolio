b199fd217a9b039274d9875660e6c2db0f75371b
wildan <wildanzhaf@gmail.com>
2026-03-20 19:02:50 +0700

    refactor: update entry point for new app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './app/App.tsx';
-import { ThemeProvider } from './app/providers/ThemeProvider.tsx';
-import { initThemeFromStorage } from './app/store/ui.store';
-
-initThemeFromStorage();
+import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
